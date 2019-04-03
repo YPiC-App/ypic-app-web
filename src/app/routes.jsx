deaab4cb80c2c56a011bc94a16c955d1e2310d26
Lyle Classen <lylec@globalkinetic.com>
2019-04-03 16:40:08 +0200

    Route Code Splitting

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Page1 from 'pages/page1';
-import Page2 from 'pages/page2';
+// import Page1 from 'pages/page1';
+// import Page2 from 'pages/page2';
+const Page1 = lazy(() => import('pages/page1'));
+const Page2 = lazy(() => import('pages/page2'));
 
 const Routes = () => (
-  <Switch>
-    <Route path="/" exact component={Page1} />
-    <Route path="/page2" component={Page2} />
-  </Switch>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+      <Route path="/" exact component={Page1} />
+      <Route path="/page2" component={Page2} />
+    </Switch>
+  </Suspense>
 );
 
 export default Routes;
