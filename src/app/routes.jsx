a5d9cc8388033acf40f1acc442807afafd81890c
Lyle Classen <lylec@globalkinetic.com>
2019-04-03 16:49:27 +0200

    clean up route code splitting

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,8 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// import Page1 from 'pages/page1';
-// import Page2 from 'pages/page2';
 const Page1 = lazy(() => import('pages/page1'));
 const Page2 = lazy(() => import('pages/page2'));
 
