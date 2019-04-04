b1d604bac1f46b0d1dfc62b2b55fdc06d6501b2a
Lyle Classen <lylec@globalkinetic.com>
2019-04-04 14:49:15 +0200

    Pre-styles

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,13 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-const Page1 = lazy(() => import('pages/page1'));
+const SignIn = lazy(() => import('pages/sign-in'));
 const Page2 = lazy(() => import('pages/page2'));
 
 const Routes = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
-      <Route path="/" exact component={Page1} />
+      <Route path="/" exact component={SignIn} />
       <Route path="/page2" component={Page2} />
     </Switch>
   </Suspense>
