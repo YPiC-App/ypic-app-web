88d9e3f952b82511a453417c358099657852baef
Lyle Classen <lylec@globalkinetic.com>
2019-04-10 13:42:31 +0200

    Added Connected-Router and Redux

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from 'reducers/store';
 import Routes from './routes';
 
 import 'styles/global.scss';
 
+const store = configureStore();
+
 const App = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </ConnectedRouter>
+  </Provider>
 );
 
 export default App;
