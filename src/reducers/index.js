64af4877e42b6f637c3398bd9c9acaf461716520
Lyle Classen <lylec@globalkinetic.com>
2019-04-15 10:15:00 +0200

    removed Connected Router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,11 @@
 import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
 
 import api from './api';
 import user from './user';
 import mainReducer from './main';
 
-export default history =>
-  combineReducers({
-    router: connectRouter(history),
-    api,
-    user,
-    main: mainReducer,
-  });
+export default combineReducers({
+  api,
+  user,
+  main: mainReducer,
+});
