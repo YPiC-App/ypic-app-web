e4d19a661d8300e36c8395628a42d891eb7fd46e
Lyle Classen <lylec@globalkinetic.com>
2019-04-24 18:54:02 +0200

    Added a loader between logged in pages

--- src/pages/main/page-routes.jsx
+++ src/pages/main/page-routes.jsx
@@ -1,14 +1,17 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import LoadingScreen from 'components/loading-screen';
 
 const Home = lazy(() => import('pages/page2'));
 const UserProfile = lazy(() => import('pages/user-profile'));
 
 const PageRoutes = () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/profile" exact component={UserProfile} />
-  </Switch>
+  <Suspense fallback={<LoadingScreen />}>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" exact component={UserProfile} />
+    </Switch>
+  </Suspense>
 );
 
 export default PageRoutes;
