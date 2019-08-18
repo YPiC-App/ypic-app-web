8143877a0d6abc39888e663d4c4cffb800f3ff3a
Lyle Classen <lylec@globalkinetic.com>
2019-08-18 12:46:20 +0200

    removed material mdc and updated style lint

--- src/components/button/index.js
+++
@@ -1,5 +0,0 @@
-import Button from '@material/react-button';
-
-import './styles.scss';
-
-export default Button;
--- src/pages/page1/index.jsx
+++ src/pages/page1/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from 'components/button';
 
 const Page1 = () => (
   <div>
     <h1>Page 1</h1>
     <Link to="/page2">Go to Page 2</Link>
-    <Button unelevated>A Very Cool Button</Button>
+    <button type="button">A Very Cool Button</button>
   </div>
 );
 
